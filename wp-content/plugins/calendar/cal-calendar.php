e7e51713aa87703e878e7b976fb72e904542b7b4
AmandineMauguin <amandine.mauguin@gmail.com>
2022-02-14 12:03:02 +0100

    création du dossier plugin calendar

---
+++ wp-content/plugins/calendar/cal-calendar.php
@@ -0,0 +1,21 @@
+<?php
+ 
+/*
+ 
+Plugin Name: Calendar
+ 
+Plugin URI: 
+ 
+Description: Affiche un calendrier avec les évènements commun. Permet de sélectionner ces disponibilité. Calcul de la date qui rassemble le plus de monde.
+ 
+Version: 1.0
+ 
+Author: Amandine Mauguin
+ 
+Author URI: //
+ 
+License: GPLv2 or later
+ 
+Text Domain: tutsplus
+ 
+*/
